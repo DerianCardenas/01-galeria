03593390b0a479b8fc78fd0a49ef1f254e75f208
Derian Cardenas <cardenasmorteraderian@gmail.com>
2023-08-22 16:12:02 -0600

    correccion de instalaciones y de compilado

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/01-galeria/', // reemplaza 'nombre_del_repositorio' con el nombre de tu repositorio en GitHub
 })
